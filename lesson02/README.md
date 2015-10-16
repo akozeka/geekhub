b0fb21090e2b3f97f4b64f28bb062482b1a2bebc
Alexey Kozeka <kozeka.alex@gmail.com>
2015-10-16 19:12:11 +0300

    Moved lesson2 task description to README.

---
+++ lesson02/README.md
@@ -0,0 +1,4 @@
+Task:
+1) Parse yml file and get file name prefix for new files
+2) Copy original test.yml as many times as specified in first commandline parameter
+3) Use Brick/Math to play with PHP float numbers funny stuff
--- lesson02/src/test.php
+++ lesson02/src/test.php
@@ -1,8 +1,4 @@
 <?php
-// Task:
-// 1) Parse yml file and get file name prefix for new files
-// 2) Copy original test.yml as many times as specified in first commandline parameter
-// 3) Use Brick/Math to play with PHP float numbers funny stuff
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
